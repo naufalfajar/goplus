974f257f471e2616fa5ace32c3d83d8ea254d782
naufalfajar <naufalphew@gmail.com>
2023-03-15 13:35:03 +0700

    add onboarding activity

---
+++ app/src/main/java/id/naufalfajar/go/view/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package id.naufalfajar.go.view.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.naufalfajar.go.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
