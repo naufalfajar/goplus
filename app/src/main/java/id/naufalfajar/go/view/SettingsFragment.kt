5945ed637f0fba0cea9227c8e9652ffe9379ccdb
naufalfajar <naufalphew@gmail.com>
2023-03-31 07:13:00 +0700

    add history, schedule, go+, and settings fragment

---
+++ app/src/main/java/id/naufalfajar/go/view/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package id.naufalfajar.go.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.naufalfajar.go.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+}
