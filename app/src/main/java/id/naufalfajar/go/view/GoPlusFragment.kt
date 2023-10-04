c65902b61ad39e3a9c5d912d365c8363158b2f9d
naufalfajar <naufalphew@gmail.com>
2023-10-04 08:03:50 +0700

    update request to coroutine scope

--- app/src/main/java/id/naufalfajar/go/view/GoPlusFragment.kt
+++ app/src/main/java/id/naufalfajar/go/view/GoPlusFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
@@ -18,23 +19,30 @@ import id.naufalfajar.go.databinding.FragmentGoPlusBinding
 import id.naufalfajar.go.model.Place
 import id.naufalfajar.go.view.detection.TextToSpeechHelper
 import id.naufalfajar.go.view.navigate.NavigationActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class GoPlusFragment : Fragment() {
     private var _binding: FragmentGoPlusBinding? = null
     private val binding get() = _binding!!
-    private var db = Firebase.firestore
     private lateinit var placeList: ArrayList<Place>
     private lateinit var placeSearch: MutableMap<String, GeoPoint>
+    private lateinit var db: FirebaseFirestore
     private lateinit var textToSpeechHelper: TextToSpeechHelper
-    private var opened: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentGoPlusBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        db = Firebase.firestore
     }
 
     override fun onDestroy() {
@@ -43,98 +51,48 @@ class GoPlusFragment : Fragment() {
         textToSpeechHelper.shutdown()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        textToSpeechHelper = TextToSpeechHelper(requireContext())
-        addStop()
-        getData()
-        onBack()
-        moveToNavigate()
-    }
-
-    private fun getData(){
-        placeList = arrayListOf()
-        placeSearch = mutableMapOf()
-        db = FirebaseFirestore.getInstance()
-        db.collection("place").get()
-            .addOnSuccessListener {
-                if(!it.isEmpty){
-                    for(data in it.documents){
-                        val dataPlace = data.toObject(Place::class.java)
-                        if(dataPlace != null){
-                            placeList.add(dataPlace)
-                            placeSearch[dataPlace.name!!] = dataPlace.location!!
-                        }
-                    }
-                }
-                binding.rvPlace.apply {
-                    adapter = GoPlaceAdapter(placeList)
-                    layoutManager = LinearLayoutManager(requireContext())
-                }
-            }
-            .addOnCompleteListener {
-                binding.pbRvPlace.visibility = View.GONE
-            }
-            .addOnFailureListener {
-                Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_LONG).show()
-            }
-    }
-
     private fun onBack(){
         binding.materialToolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
     }
 
-    private fun addStop(){
-        binding.apply {
-            tvTambahPemberhentian.setOnClickListener {
-                if(!opened){
-                    dotLine2.visibility = View.VISIBLE
-                    dotLocation2.visibility = View.VISIBLE
-                    etPemberhentian2.visibility = View.VISIBLE
-                    dotLine2.alpha = 0f
-                    dotLocation2.alpha = 0f
-                    etPemberhentian2.alpha = 0f
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textToSpeechHelper = TextToSpeechHelper(requireContext())
+        getData()
+        onBack()
+        moveToNavigate()
+    }
 
-                    dotLine2.animate()
-                        .translationY(0f)
-                        .alpha(1.0f)
-                        .setDuration(500)
-                        .setListener(null)
+    private fun getData() {
+        placeList = arrayListOf()
+        placeSearch = mutableMapOf()
 
-                    dotLocation2.animate()
-                        .translationY(0f)
-                        .alpha(1.0f)
-                        .setDuration(750)
-                        .setListener(null)
+        binding.pbRvPlace.visibility = View.VISIBLE
 
-                    etPemberhentian2.animate()
-                        .translationY(0f)
-                        .alpha(1.0f)
-                        .setDuration(1000)
-                        .setListener(null)
+        lifecycleScope.launch {
+            try {
+                val querySnapshot = withContext(Dispatchers.IO) {
+                    db.collection("place").get().await()
                 }
-                else{
-                    dotLine2.animate()
-                        .translationY(-dotLine2.height.toFloat())
-                        .alpha(0.0f)
-                        .setDuration(500)
-                        .setListener(null)
 
-                    dotLocation2.animate()
-                        .translationY(-dotLocation2.height.toFloat())
-                        .alpha(0.0f)
-                        .setDuration(500)
-                        .setListener(null)
-
-                    etPemberhentian2.animate()
-                        .translationY(-etPemberhentian2.height.toFloat())
-                        .alpha(0.0f)
-                        .setDuration(750)
-                        .setListener(null)
+                for (data in querySnapshot.documents) {
+                    val dataPlace = data.toObject(Place::class.java)
+                    dataPlace?.let {
+                        placeList.add(it)
+                        placeSearch[it.name!!] = it.location!!
+                    }
                 }
-                opened = !opened
+
+                binding.rvPlace.apply {
+                    adapter = GoPlaceAdapter(placeList)
+                    layoutManager = LinearLayoutManager(requireContext())
+                }
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), e.toString(), Toast.LENGTH_LONG).show()
+            } finally {
+                binding.pbRvPlace.visibility = View.GONE
             }
         }
     }
@@ -143,8 +101,8 @@ class GoPlusFragment : Fragment() {
         binding.mbtnGo.setOnClickListener {
             val input = binding.etPemberhentian1.text
             var count = 0
-            var lng : Double = 0.0
-            var lat : Double = 0.0
+            var lng = 0.0
+            var lat = 0.0
             if(placeSearch.isNotEmpty()){
                 for (entry in placeSearch) {
                     if (entry.key.lowercase().contains(input.toString().lowercase())) {
@@ -157,14 +115,11 @@ class GoPlusFragment : Fragment() {
                     val intent = Intent(requireContext(), NavigationActivity::class.java)
                     intent.putExtra("latitude", lat)
                     intent.putExtra("longitude", lng)
-                    // start your next activity
                     startActivity(intent)
                 } else {
                     textToSpeechHelper.speak("Input anda kurang tepat")
                 }
             }
-//            val intent = Intent(requireContext(), NavigationActivity::class.java)
-//            startActivity(intent)
         }
     }
 }
